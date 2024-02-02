75622dc3ecb587efc4c14b1c4bf760b30940c11f
Paris Parker <130878057+ParisAParker@users.noreply.github.com>
2024-02-01 21:38:50 -0600

    Started question 12

--- notebooks/usa_names.sql
+++ notebooks/usa_names.sql
@@ -6,11 +6,10 @@ FROM names;
 SELECT SUM(num_registered) as sum
 FROM names;
 
--- Question 3: James
-SELECT name, SUM(num_registered)
+-- Question 3: Linda
+SELECT name, num_registered, year
 FROM names
-GROUP BY name
-ORDER BY sum DESC;
+ORDER BY num_registered DESC;
 
 -- Question 4: 1880 - 2018
 SELECT MIN(year), MAX(year)
@@ -25,4 +24,56 @@ GROUP BY year;
 SELECT DISTINCT name
 FROM names;
 
--- Question 7: 
+-- Question 7: More males registered
+SELECT gender, SUM(num_registered)
+FROM names
+GROUP BY gender;
+
+-- Question 8: Top female names are Mary, Elizabeth, and Patricia & Top male names are James, John, Robert
+SELECT name, sum(num_registered)
+FROM names
+WHERE gender = 'F'
+GROUP BY name
+ORDER by sum DESC
+LIMIT 5;
+
+SELECT name, sum(num_registered)
+FROM names
+WHERE gender = 'M'
+GROUP BY name
+ORDER BY sum DESC
+LIMIT 5;
+
+-- Question 9: Jacob & Emily
+SELECT name, sum(num_registered)
+FROM names
+WHERE year BETWEEN 2000 AND 2009
+AND gender = 'M'
+GROUP BY name
+ORDER BY sum DESC;
+
+SELECT name, sum(num_registered)
+FROM names
+WHERE year BETWEEN 2000 AND 2009
+AND gender = 'F'
+GROUP BY name
+ORDER BY sum DESC;
+
+-- Question 10: 2008
+SELECT COUNT(DISTINCT name), year
+FROM names
+GROUP BY year
+ORDER BY count DESC;
+
+-- Question 11: Ximena
+SELECT name, sum(num_registered)
+FROM names
+WHERE gender = 'F'
+AND name LIKE 'X%'
+GROUP BY name
+ORDER BY sum DESC;
+
+-- Question 12: 
+SELECT DISTINCT name
+FROM names
+
